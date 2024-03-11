a911b26290cc547b635c86688b0a8d7d5b00ab7e
Ajay Rathod <128123668+ajayrathod0@users.noreply.github.com>
2024-03-11 18:54:00 +0530

    Update Student.aspx.cs

--- ADONETWebApp/Student.aspx.cs
+++ ADONETWebApp/Student.aspx.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using System.Configuration;
 using System.Data.SqlClient;
-=======
->>>>>>> 0cd43fec0fba19d2264ac734a27ff4cdedd033bd
 using System.Linq;
 using System.Web;
 using System.Web.UI;
